95789e828d51b5c2a765294179ac237914cb6bc2
bravo951 <lockerrysep@gmail.com>
2021-09-21 20:59:16 -0400

    C# programming problems

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {   
+        //public void add(int a,int b)
+        //{
+        //    Console.WriteLine(a + b);
+        //}
+        static void Main(string[] args)
+        {
+            Sol1 sol = new Sol1();
+            Console.WriteLine(sol.factorial(3));
+            Console.WriteLine(sol.isPrime(18));
+            Console.WriteLine(sol.isLeap(1996));
+            Console.WriteLine(sol.LCM(105,350));
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Sol1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Sol1
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public int factorial(int a)
+        {
+            if (a <= 1) return 1;
+            return a * factorial(a - 1);
+        }
+        public bool isPrime(int a)
+        {
+            if (a < 2) return false;
+            int up = (int)Math.Sqrt(a);
+            for (int i = 2; i <= up; i++)
+            {
+                if (a % i == 0) return true;
+            }
+            return false;
+        }
+        public bool isLeap(int year)
+        {
+            return (year - 2008) % 4 == 0;
+        }
+        public int LCM(int a,int b)
+        {
+            int c;
+            if (a < b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            c = a % b;
+            if (c == 0) return b;
+            return LCM(b, c);
+        }
+    }
+}
